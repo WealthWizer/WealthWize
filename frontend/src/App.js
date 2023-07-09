44e1a5b8565b34a686314152800481158b60fa06
Michael Weckop <michaelweckop@michaels-air-2.lan>
2023-07-08 20:41:07 -0400

    Navbar component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import LoginPage from "./pages/LoginPage";
-import {Routes,Route,BrowserRouter} from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Dashboard from "./pages/dashboard";
 import Signup from "./pages/signup";
 import "./index.css";
+import { use } from "bcrypt/promises";
 function App() {
-  const [isLoggedIn, setIsLoggedIn]= useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // const [isSignedUp, setisSignedUp]=useState(false);
+  const [username, setUsername] = useState('user 1');
 
   //add authProvider later, might be unneccessary 
   return (
-<>
-<BrowserRouter>
-      <Routes>
+    <>
+      <BrowserRouter>
+        <Routes>
 
-      <Route path='/' element={<LoginPage/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      </Routes>
-</BrowserRouter>
-</>
- 
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/dashboard' element={<Dashboard username={username} setIsLoggedIn={setIsLoggedIn}/>} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
