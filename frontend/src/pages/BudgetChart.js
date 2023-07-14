132b5bd707ab51858bcdb5f9583c558bc143b22d
Vicky Liu <vlpubi@gmail.com>
2023-07-13 21:12:11 -0500

    budget component functionality all there, styling half done

--- frontend/src/pages/BudgetChart.js
+++ frontend/src/pages/BudgetChart.js
@@ -1,84 +1,3 @@
-// import React, { useState,useEffect, useContext } from 'react';
-// import Chart from "react-apexcharts";
-
-
-//       const BudgetChart = ({ totalBudget, sumTrans, transactions }) => {
-//         const [chartvals, setChartvals] = useState({
-//           chart: {
-//             type: 'line',
-//             height: 350,
-//           },
-//           dataLabels: {
-//             enabled: false,
-//           },
-//           stroke: {
-//             curve: 'smooth',
-//           },
-//           series: [
-//             {
-//               name: 'cumulative',
-//               data: [], // Will be filled with cumulative transaction data
-//             },
-//             {
-//               name: 'daily',
-//               data: [], // Will be filled with daily transaction data
-//             },
-//             {
-//               name: 'budget',
-//               data: [totalBudget, totalBudget],
-//             },
-//           ],
-//           xaxis: {
-//             categories: [], // Will be filled with dates
-//           },
-//           yaxis: {
-//             axisTicks: {
-//               show: true,
-//             },
-//             axisBorder: {
-//               show: true,
-//             },
-//           },
-//         });
-      
-//         useEffect(() => {
-//         const updategraph= async()=>{
-//             try{
-//   // Format cumulative transaction data
-//   const cumulativeData=Object.values(sumTrans);
-//   // Format daily transaction data
-//   const dailyData = Object.values(transactions);
-
-//   // Get dates from the transaction objects
-//   const dates = Object.keys(transactions);
-
-//   setChartvals((prevChartVals) => ({
-//     ...prevChartVals,
-//     series: [
-//       { ...prevChartVals.series[0], data: cumulativeData },
-//       { ...prevChartVals.series[1], data: dailyData },
-//     ],
-//     xaxis: { ...prevChartVals.xaxis, categories: dates },
-//   }));
-//             }
-//             catch(err){
-//                 console.log(err);
-//             }
-//         }
-//         updategraph();
-//         }, [transactions]);
-
-//     return(
-//         <div className='budgetchart'>
-//             <Chart
-//             options={chartvals}
-//             series={chartvals.series}/>
-//         </div>
-//     )
-// }
-// export default BudgetChart;
-
-
 
 import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
@@ -87,10 +6,21 @@ const BudgetChart = ({ totalBudget, sumTrans, transactions }) => {
   const [chartvals, setChartvals] = useState({
     chart: {
       type: 'line',
-      height: 350,
+        width: 600,
+        height:400,
+        dataLabels:{
+            position:'bottom',
+            align:'right'
+        }
+
+        // redrawOnParentResize: true
     },
     dataLabels: {
       enabled: false,
+    },
+    legend: {
+        show: false,
+        position: 'bottom'
     },
     stroke: {
       curve: 'smooth',
@@ -106,11 +36,35 @@ const BudgetChart = ({ totalBudget, sumTrans, transactions }) => {
       },
       {
         name: 'budget',
-        data: [totalBudget, totalBudget],
+        data: [totalBudget, totalBudget, totalBudget],
       },
+
     ],
+    // fill:{
+    //     type:'gradient',
+    //     gradient:{
+    //         shadeIntensity:1,
+    //         opacityfrom:0.7,
+    //         opacityTo:0.9,
+    //         colorStops:function(data){
+    //                 return data.map((d,idx))=>{
+    //                     let color=d>0?'#22c55f':'#ef4544';
+    //                     return{
+    //                         offset:idx/data.length*100
+    //                     }
+    //                 }
+    //         }
+    //     }
+    // },
     xaxis: {
       categories: [], // Will be filled with dates
+      type: 'datetime',
+      tickAmount:9,
+      labels:{
+        formatter:function(val,timestamp,opts){
+            return opts.dateFormatter(new Date(timestamp),'dd MMM')
+        }
+      }
     },
     yaxis: {
       axisTicks: {
@@ -126,19 +80,30 @@ const BudgetChart = ({ totalBudget, sumTrans, transactions }) => {
     const updateGraph = async () => {
       try {
         // Format cumulative transaction data
+        const dates = Object.keys(sumTrans);
         const cumulativeData = Object.values(sumTrans);
 
         // Format daily transaction data
-        const dailyData = Object.values(transactions);
+        // const dailyData = Object.values(transactions);
+        const dailyData=[];
+        const budgetData=[];
 
-        // Get dates from the transaction objects
-        const dates = Object.keys(transactions);
+        for(let i of dates){
+            if(!transactions[i]){
+                dailyData.push(0);
+            }else{
+
+                dailyData.push(transactions[i]);
+            }
+            budgetData.push(totalBudget);
+        }
 
         setChartvals((prevChartVals) => ({
           ...prevChartVals,
           series: [
             { ...prevChartVals.series[0], data: cumulativeData },
             { ...prevChartVals.series[1], data: dailyData },
+            {...prevChartVals.series[2],data: budgetData}
           ],
           xaxis: { ...prevChartVals.xaxis, categories: dates },
         }));
@@ -152,7 +117,7 @@ const BudgetChart = ({ totalBudget, sumTrans, transactions }) => {
 
   return (
     <div className="budgetchart">
-      <Chart options={chartvals} series={chartvals.series} type="line" height={chartvals.chart.height} />
+      <Chart options={chartvals} series={chartvals.series} type="line" height="500" width="600" />
     </div>
   );
 };
