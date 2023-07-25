5a425cd5d5bcea8db3b3cfb3251adde1e764ffa0
KW-SWE <50188099+KW-SWE@users.noreply.github.com>
2023-07-24 18:18:52 -0600

    Removed and added files to properly run npm start and display the dashboard

---
+++ backend/routes/stockRoutes.js
@@ -0,0 +1,8 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const stockController = require("../controller/stockController");
+
+// router.get('/:ticker', stockController.getStockData);
+
+// module.exports = router;
